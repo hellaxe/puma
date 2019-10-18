c994a84f62a98bcbed655966e9de0ad2cd5c2f1a
Wadim <helaxe@gmail.com>
2019-10-19 00:46:29 +0300

    added env to parse_error method

--- lib/puma/events.rb
+++ lib/puma/events.rb
@@ -93,10 +93,8 @@ module Puma
     # parsing exception.
     #
     def parse_error(server, env, error)
-      @stderr.puts "#{Time.now}: HTTP parse error, malformed request " \
-        "(#{env[HTTP_X_FORWARDED_FOR] || env[REMOTE_ADDR]}#{env[REQUEST_PATH]}): " \
-        "#{error.inspect}" \
-        "\n---\n"
+      @stderr.puts "#{Time.now}: HTTP parse error, malformed request (#{env[HTTP_X_FORWARDED_FOR] || env[REMOTE_ADDR]}): #{error.inspect}"
+      @stderr.puts "#{Time.now}: ENV: #{env.inspect}\n---\n"
     end
 
     # An SSL error has occurred.
